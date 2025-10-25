7ba12a468d9022c0c0f1b0709dd0f0627dc131e3
pingfish9173 <pingfish9173@gmail.com>
2025-10-25 21:33:36 +0800

    firebase-admin安裝完成，測試部署

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.champion.king"  // ← 改成你原本的 applicationId
         minSdk = 24                          // ← 依你原本設定
         targetSdk = 34                       // ← 依你原本設定
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
