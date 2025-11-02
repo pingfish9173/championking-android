ebab011156ccaa0e98b6b65d4ba6ad01ab5394cc
pingfish9173 <pingfish9173@gmail.com>
2025-11-02 13:59:49 +0800

    chore: 自動發布版本 v1.1.1 (build 45)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.champion.king"
         minSdk = 24
         targetSdk = 34
-        versionCode = 44
-        versionName = "1.1.0"
+        versionCode = 45
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
