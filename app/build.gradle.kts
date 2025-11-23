410bc7b8cde5af6e1320f5535e38645b8dfa9cd3
pingfish9173 <pingfish9173@gmail.com>
2025-11-23 21:31:23 +0800

    chore: 自動發布版本 v1.1.26 (build 70)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.champion.king"
         minSdk = 24
         targetSdk = 34
-        versionCode = 69
-        versionName = "1.1.25"
+        versionCode = 70
+        versionName = "1.1.26"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
