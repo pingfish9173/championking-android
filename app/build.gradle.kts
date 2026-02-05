875242a5eee95530aede470a38ef47b6ab99b640
pingfish9173 <pingfish9173@gmail.com>
2026-02-05 22:26:18 +0800

    chore: 自動發布版本 v1.1.51 (build 95)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.champion.king"
         minSdk = 24
         targetSdk = 34
-        versionCode = 94
-        versionName = "1.1.50"
+        versionCode = 95
+        versionName = "1.1.51"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
