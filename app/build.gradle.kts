e9fa4b0dd43309d4454ff83613dd7272ae978465
pingfish9173 <pingfish9173@gmail.com>
2026-03-22 21:00:48 +0800

    chore: 自動發布版本 v1.3.3 (build 102)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.champion.king"
         minSdk = 24
         targetSdk = 34
-        versionCode = 101
-        versionName = "1.3.2"
+        versionCode = 102
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
