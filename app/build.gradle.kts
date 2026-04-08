25d6839cc613eb2bb0b0570a46f0bc3aa316f977
pingfish9173 <pingfish9173@gmail.com>
2026-04-08 23:15:19 +0800

    chore: 自動發布版本 v1.3.5 (build 104)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.champion.king"
         minSdk = 24
         targetSdk = 34
-        versionCode = 103
-        versionName = "1.3.4"
+        versionCode = 104
+        versionName = "1.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
